f16cc3c07ada62f920f4dcd2f9be8fa2db4497b4
Mir Drualga <IAmRealTrial@gmail.com>
2021-08-30 00:21:09 -0700

    Assign defaults to cel file config default

---
+++ SGD2FreeResolution/src/cel_file_config/cel_file_config_struct.c
@@ -0,0 +1,125 @@
+/**
+ * SlashGaming Diablo II Free Resolution
+ * Copyright (C) 2019-2021  Mir Drualga
+ *
+ * This file is part of SlashGaming Diablo II Free Resolution.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Additional permissions under GNU Affero General Public License version 3
+ *  section 7
+ *
+ *  If you modify this Program, or any covered work, by linking or combining
+ *  it with Diablo II (or a modified version of that game and its
+ *  libraries), containing parts covered by the terms of Blizzard End User
+ *  License Agreement, the licensors of this Program grant you additional
+ *  permission to convey the resulting work. This additional permission is
+ *  also extended to any combination of expansions, mods, and remasters of
+ *  the game.
+ *
+ *  If you modify this Program, or any covered work, by linking or combining
+ *  it with any Graphics Device Interface (GDI), DirectDraw, Direct3D,
+ *  Glide, OpenGL, or Rave wrapper (or modified versions of those
+ *  libraries), containing parts not covered by a compatible license, the
+ *  licensors of this Program grant you additional permission to convey the
+ *  resulting work.
+ *
+ *  If you modify this Program, or any covered work, by linking or combining
+ *  it with any library (or a modified version of that library) that links
+ *  to Diablo II (or a modified version of that game and its libraries),
+ *  containing parts not covered by a compatible license, the licensors of
+ *  this Program grant you additional permission to convey the resulting
+ *  work.
+ */
+
+#include "cel_file_config_struct.h"
+
+#include "cel_file_config_value_default.h"
+
+const struct CelFileConfig CelFileConfig_kDefault = {
+    /* Interface bar background left */
+    {
+        CEL_FILE_CONFIG_INTERFACE_BAR_BACKGROUND_LEFT_PATH_DEFAULT,
+    },
+
+    /* Interface bar background left-center */
+    {
+        CEL_FILE_CONFIG_INTERFACE_BAR_BACKGROUND_LEFT_CENTER_PATH_DEFAULT,
+    },
+
+    /* Interface bar background right */
+    {
+        CEL_FILE_CONFIG_INTERFACE_BAR_BACKGROUND_RIGHT_PATH_DEFAULT,
+    },
+
+    /* Interface bar background right-center */
+    {
+        CEL_FILE_CONFIG_INTERFACE_BAR_BACKGROUND_RIGHT_CENTER_PATH_DEFAULT,
+    },
+
+    /* Life orb interface */
+    {
+        CEL_FILE_CONFIG_LIFE_ORB_INTERFACE_PATH_DEFAULT,
+    },
+
+    /* Life orb red */
+    {
+        CEL_FILE_CONFIG_LIFE_ORB_RED_PATH_DEFAULT,
+    },
+
+    /* Life orb green */
+    {
+        CEL_FILE_CONFIG_LIFE_ORB_GREEN_PATH_DEFAULT,
+    },
+
+    /* Mana orb interface */
+    {
+        CEL_FILE_CONFIG_MANA_ORB_INTERFACE_PATH_DEFAULT,
+    },
+
+    /* Mana orb blue */
+    {
+        CEL_FILE_CONFIG_MANA_ORB_BLUE_PATH_DEFAULT,
+    },
+
+    /* Left screen background */
+    {
+        CEL_FILE_CONFIG_LEFT_SCREEN_BACKGROUND_PATH_DEFAULT,
+    },
+
+    /* Right screen background */
+    {
+        CEL_FILE_CONFIG_RIGHT_SCREEN_BACKGROUND_PATH_DEFAULT,
+    },
+
+    /* Left screen background border */
+    {
+        CEL_FILE_CONFIG_LEFT_SCREEN_BACKGROUND_BORDER_PATH_DEFAULT,
+    },
+
+    /* Right screen background border */
+    {
+        CEL_FILE_CONFIG_RIGHT_SCREEN_BACKGROUND_BORDER_PATH_DEFAULT,
+    },
+
+    /* Left screen border */
+    {
+        CEL_FILE_CONFIG_LEFT_SCREEN_BORDER_PATH_DEFAULT,
+    },
+
+    /* Right screen border */
+    {
+        CEL_FILE_CONFIG_RIGHT_SCREEN_BORDER_PATH_DEFAULT,
+    },
+};
