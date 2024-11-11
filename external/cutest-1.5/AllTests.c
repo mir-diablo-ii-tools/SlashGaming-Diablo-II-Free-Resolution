4d781cf5135e316d99fe970179bed561a3439529
Mir Drualga <IAmRealTrial+SGD2FreeRes@gmail.com>
2024-11-10 17:49:25 -0800

    Add CuTest 1.5
    CuTest is able to work on VC6, and is C89 compliant. No changes are
    required to make it run. A CMakeLists file is added to allow integration
    with CMake.

---
+++ external/cutest-1.5/AllTests.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#include "CuTest.h"
+
+CuSuite* CuGetSuite();
+CuSuite* CuStringGetSuite();
+
+void RunAllTests(void)
+{
+	CuString *output = CuStringNew();
+	CuSuite* suite = CuSuiteNew();
+
+	CuSuiteAddSuite(suite, CuGetSuite());
+	CuSuiteAddSuite(suite, CuStringGetSuite());
+
+	CuSuiteRun(suite);
+	CuSuiteSummary(suite, output);
+	CuSuiteDetails(suite, output);
+	printf("%s\n", output->buffer);
+}
+
+int main(void)
+{
+	RunAllTests();
+
+	
+	getchar();
+}
