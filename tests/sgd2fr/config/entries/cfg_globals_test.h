93eb059cea4ccbf6f37de70a2acbcceb225a2b67
Mir Drualga <IAmRealTrial+SGD2FreeRes@gmail.com>
2024-11-23 16:36:28 -0800

    Add CfgGlobals type to store globals values

--- tests/sgd2fr/config/entries/cfg_globals_test.h
+++ tests/sgd2fr/config/entries/cfg_globals_test.h
@@ -43,27 +43,19 @@
  *  work.
  */
 
-#include <stdio.h>
+#ifndef SGD2FR_CONFIG_ENTRIES_CFG_GLOBALS_TEST_H_
+#define SGD2FR_CONFIG_ENTRIES_CFG_GLOBALS_TEST_H_
 
 #include <CuTest.h>
 
-#include "sgd2fr/config/entries/cfg_globals_test.h"
-#include "sgd2fr/config/entries/globals/cfg_indent_width_test.h"
+#ifdef __cplusplus
+extern "C" {
+#endif  /* __cplusplus */
 
-static void RunAllTests(void) {
-  CuString *output = CuStringNew();
-  CuSuite* suite = CuSuiteNew();
+CuSuite* CfgGlobals_GetTestSuite(void);
 
-  CuSuiteAddSuite(suite, CfgGlobals_GetTestSuite());
-  CuSuiteAddSuite(suite, CfgIndentWidth_GetTestSuite());
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif  /* __cplusplus */
 
-  CuSuiteRun(suite);
-  CuSuiteSummary(suite, output);
-  CuSuiteDetails(suite, output);
-  printf("%s\n", output->buffer);
-}
-
-int main() {
-  RunAllTests();
-  return 0;
-}
+#endif  /* SGD2FR_CONFIG_ENTRIES_CFG_GLOBALS_TEST_H_ */
